ed566e4016c320173dec39bcc062822d628a00cf
snehathakor048-maker <snehathakor048@gmail.com>
2026-01-30 21:25:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
 
